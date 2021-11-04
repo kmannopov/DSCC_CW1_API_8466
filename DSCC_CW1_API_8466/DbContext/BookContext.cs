16847588deefda72b95385db50a2194f945a55f7
kmannopov <kmannopov@outlook.com>
2021-11-03 18:48:06 -0700

    added dbcontext

---
+++ DSCC_CW1_API_8466/DbContext/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using DSCC_CW1_MicroservicesAPI_8466.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC_CW1_MicroservicesAPI_8466.DbContexts
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
