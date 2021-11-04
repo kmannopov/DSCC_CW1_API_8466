2c8882c5bd45267020c8d7b133acbd073dfed4b3
kmannopov <kmannopov@outlook.com>
2021-11-03 18:44:44 -0700

    Add project files.

---
+++ DSCC_CW1_API_8466/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC_CW1_MicroservicesAPI_8466.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Summary { get; set; }
+        public int Isbn { get; set; }
+    }
+}
